695eca1ba386736c8a9db0045ed87e2d068edac6
Paul Hamilton <phamtec@mac.com>
2024-05-27 14:21:26 +1000

    Initial checkin.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # flo
+
 f(lo)
+
+## Building and testing
+
+Build this project:
+
+```
+git clone https://github.com/visualops/flo.git
+cd flo
+mkdir build
+cd build
+cmake ..
+make
+make test
+```
---
+++ test/simpletest.cpp
@@ -0,0 +1,24 @@
+/*
+  simpletest.cpp
+  
+  Author: Paul Hamilton (paul@visualops.com)
+  Date: 27-May-2024
+  
+  Licensed under [version 3 of the GNU General Public License] contained in LICENSE.
+ 
+  https://github.com/visualopsholdings/Transceiver
+*/
+
+#include <iostream>
+
+#define BOOST_AUTO_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+using namespace std;
+
+BOOST_AUTO_TEST_CASE( hello )
+{
+  cout << "=== hello ===" << endl;
+  
+  
+}
